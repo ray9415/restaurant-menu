0bac2280ec5087ca62d21ea5e71c95e177838d5a
rayh <raycjhuang415@gmail.com>
2023-05-22 10:23:11 +0800

    add controller

---
+++ restaurant/restaurant/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace restaurant.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
