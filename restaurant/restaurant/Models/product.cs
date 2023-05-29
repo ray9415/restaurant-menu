ae6da59745e8f2bb8f8e1bf5bcd3710a49424033
DESKTOP-A1PRTOI\r0917 <raycjhuang415@gmail.com>
2023-05-29 23:34:38 +0800

    product

--- restaurant/restaurant/Models/product.cs
+++ restaurant/restaurant/Models/product.cs
@@ -8,10 +8,21 @@ namespace restaurant.Models
     public class Product
     {
         public int ProductId { get; set; }
+
+        [DisplayName("商品名稱")]
+        [Required(ErrorMessage ="品名不可為空")]
         public string Name { get; set; }
+
+        [DisplayName("類型")]
         public string Type { get; set; }
+
+        [DisplayName("描述")]
         public string Description { get; set; }
+
+        [DisplayName("單價")]
         public int Price { get; set; }
+
+        [DisplayName("圖片")]
         public string IMG { get; set; }
 
     }
