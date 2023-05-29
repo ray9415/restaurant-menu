d886238082745e9ab852bd054687d9b4713a453a
DESKTOP-A1PRTOI\r0917 <raycjhuang415@gmail.com>
2023-05-29 14:40:59 +0800

    add model

---
+++ restaurant/restaurant/Models/product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace restaurant.Models
+{
+    public class product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string IMG { get; set; }
+
+    }
+}
