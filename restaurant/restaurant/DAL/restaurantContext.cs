c33073f7be3d520872b0404807c87ec754ae1b42
DESKTOP-A1PRTOI\r0917 <raycjhuang415@gmail.com>
2023-05-31 15:31:06 +0800

    add context

---
+++ restaurant/restaurant/DAL/restaurantContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using restaurant.Models;
+
+namespace restaurant.DAL
+{
+    public class restaurantContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+    }
+}
