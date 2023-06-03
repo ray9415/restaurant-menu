7c6b2ba75521a5e7faed2e2ba86aa1a4737c61bb
DESKTOP-A1PRTOI\r0917 <raycjhuang415@gmail.com>
2023-06-03 11:34:20 +0800

    finish cn

---
+++ restaurant/restaurant/Data/restaurantContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using restaurant.Models;
+
+namespace restaurant.Data
+{
+    public class restaurantContext : DbContext
+    {
+        public restaurantContext(DbContextOptions<restaurantContext> options):base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- restaurant/restaurant/Program.cs
+++ restaurant/restaurant/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using restaurant.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<restaurantContext>(Options=>Options.UseSqlServer(builder.Configuration.GetConnectionString("DBconnection")));
 
 var app = builder.Build();
 
