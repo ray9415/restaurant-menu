2a2137e2af01eb25a316c2916093f71a3ec6d7e6
DESKTOP-A1PRTOI\r0917 <raycjhuang415@gmail.com>
2023-06-08 22:08:51 +0800

    add create

--- restaurant/restaurant/Areas/ProductManager/Controllers/ProductCRUDController.cs
+++ restaurant/restaurant/Areas/ProductManager/Controllers/ProductCRUDController.cs
@@ -1,12 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using restaurant.Models;
+using restaurant.Data;
 
 namespace restaurant.Areas.ProductManager.Controllers
 {
     [Area("ProductManager")]
     public class ProductCRUDController : Controller
     {
-        public IActionResult Index()
+        private readonly restaurantContext _context;
+        public ProductCRUDController(restaurantContext context)
         {
+            _context = context;
+        }
+        public IActionResult Index() 
+        { 
+            var result = _context.Products.ToList();
+            return View(result); 
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
             return View();
         }
     }
