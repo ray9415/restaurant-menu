77145ab71f8b3a7d1007c309487e3b998fe37cfe
DESKTOP-A1PRTOI\r0917 <raycjhuang415@gmail.com>
2023-06-11 23:34:17 +0800

    upload

--- restaurant/restaurant/Areas/ProductManager/Controllers/ProductCRUDController.cs
+++ restaurant/restaurant/Areas/ProductManager/Controllers/ProductCRUDController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+//using System.IO;
+using System.Collections;
 using restaurant.Models;
 using restaurant.Data;
+using System.Threading.Tasks;
 
 namespace restaurant.Areas.ProductManager.Controllers
 {
@@ -8,14 +11,40 @@ namespace restaurant.Areas.ProductManager.Controllers
     public class ProductCRUDController : Controller
     {
         private readonly restaurantContext _context;
-        public ProductCRUDController(restaurantContext context)
+        private readonly IWebHostEnvironment _environment ;
+        public ProductCRUDController(restaurantContext context, IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
+
         public IActionResult Index() 
         { 
             var result = _context.Products.OrderBy(m => m.Type).ToList();
             return View(result); 
+        }
+
+        [HttpPost]//上傳檔案
+        public async Task <IActionResult> Upload(IFormFile file)
+        {
+            if(file == null || file.Length == 0)
+            {
+                return BadRequest(BadRequest());
+            }
+            var filename = file.FileName;
+            var filepath = Path.Combine(_environment.WebRootPath,@"\IMGs"+filename);
+            using (var stream = new FileStream(filepath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            //檔名存入資料庫
+            if (!string.IsNullOrEmpty(filename))
+            {
+                _context.Products.Add(new Product{ IMG = filename});
+                _context.SaveChanges();
+            }                
+            return RedirectToAction("Index");
+
         }
 
         public IActionResult Create()
@@ -24,8 +53,9 @@ namespace restaurant.Areas.ProductManager.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create(Product product)
+        public async Task <IActionResult> Create(Product product , IFormFile file)
         {
+            await Upload(file);
             _context.Products.Add(product);
             _context.SaveChanges();
             return View();
