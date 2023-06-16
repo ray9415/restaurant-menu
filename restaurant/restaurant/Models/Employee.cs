2d7a090d343ec450fd92c97c6e077197b2cefdc3
DESKTOP-A1PRTOI\r0917 <raycjhuang415@gmail.com>
2023-06-16 23:43:03 +0800

    add detail view

---
+++ restaurant/restaurant/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace restaurant.Models
+{
+    public class Employee
+    {
+        [Key] public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+}
